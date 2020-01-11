c489f29b3d72e0f36ee535a98e9dde8c64fae4f9
ruitao.xu <ruitao.xu@aliyun.com>
2020-01-11 10:28:42 +0800

    chore: add description for config/config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
 export const GITHUB_OAUTH_APP = {
-  client_id: process.env.GITHUB_OAUTH_APP_CLIENT_ID,
-  client_secret: process.env.GITHUB_OAUTH_APP_CLIENT_SECRET
+  client_id:
+    process.env.GITHUB_OAUTH_APP_CLIENT_ID ||
+    "<your_github_oauth_app_client_id>",
+  client_secret:
+    process.env.GITHUB_OAUTH_APP_CLIENT_SECRET ||
+    "<your_github_oauth_app_client_secret>"
 };
 
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const APP_ROOT = process.env.APP_ROOT;
+export const JWT_SECRET = process.env.JWT_SECRET || "<your_secret_to_sign_gwt>";
+export const APP_ROOT = process.env.APP_ROOT || "<your_web_app_root>";
